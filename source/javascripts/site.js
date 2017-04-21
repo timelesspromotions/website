6a37e68f24632b214b20e56c58a1184cea7bd17f
Claudio Vallejo <hola@claudiovallejo.mx>
2017-04-21 14:44:42 -0500

    Created loading script.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -4,12 +4,12 @@ $pickups.forEach(function($pickup){
   //  Mouse over
   $pickup.addEventListener('mouseover', function(){
     removeClasses(this, ["t-translateY0", "bs-alpha"]);
-    addClasses(this,["t-translateY-0_5r", "bs-beta"]);
+    addClasses(this, ["t-translateY-0_5r", "bs-beta"]);
   });
   //  Mouse out
   $pickup.addEventListener('mouseout', function(){
-    removeClasses(this,["t-translateY-0_5r", "bs-beta"]);
-    addClasses(this,["t-translateY0", "bs-alpha"]);
+    removeClasses(this, ["t-translateY-0_5r", "bs-beta"]);
+    addClasses(this, ["t-translateY0", "bs-alpha"]);
   });
 });
 //  Fade interaction
@@ -26,6 +26,20 @@ $fades.forEach(function($fade){
     addClasses(this, ["o-1"]);
   });
 });
+//  Page load animations
+const $hero = document.querySelector('[data-load="hero"]');
+const $films = document.querySelectorAll('[data-load="film"]');
+window.addEventListener('load', function(){
+  //  Fade-in $hero
+  removeClasses($hero, ["o-0"]);
+  addClasses($hero, ["o-1"]);
+  //  Attach event listener when $hero is finished fading in
+  $hero.addEventListener('transitionend', function(){
+    $films.forEach(function($film, index){
+      delayedFade($film, index);
+    });
+  });
+});
 //  Remove list of classes from selected element
 function removeClasses($element, classes) {
   for (var i = 0; i < classes.length; i++) {
@@ -37,4 +51,10 @@ function addClasses($element, classes) {
   for (var i = 0; i < classes.length; i++) {
     $element.classList.add(classes[i]);
   }
+}
+//  Delayed fade-ins
+function delayedFade($element, index) {
+  $element.setAttribute('style', 'transition-delay: ' + ((index+1)*0.15) + 's;');
+  removeClasses($element, ["o-0", "t-translateX4r", "t-translateY4r-ns"]);
+  addClasses($element, ["o-1", "t-translateX0", "t-translateY0-ns"]);
 }
